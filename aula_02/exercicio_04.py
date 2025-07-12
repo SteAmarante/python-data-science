c5b1d0fdd69073ef2c8a0466a336fab0ef25da0b
Stephanie Amarante <89923920+SteAmarante@users.noreply.github.com>
2025-07-12 14:22:19 -0300

    Add files via upload

---
+++ aula_02/exercicio_04.py
@@ -0,0 +1,7 @@
+# Numa eleição existem três candidatos: Faça um programa que peça o número total de eleitores.
+# Peça para cada eleitor votar e ao final mostrar o número de votos de cada candidato.
+
+can1 = 0
+can2 = 0
+can3 = 0
+
